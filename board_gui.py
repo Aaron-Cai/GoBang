87b2a8e641a99e24479cce837f3f14a5de24cced
Aaron <cjy_mail@foxmail.com>
2019-06-20 21:59:12 +0800

    drawing score board on ui

--- board_gui.py
+++ board_gui.py
@@ -14,10 +14,15 @@ class GameBoard(QWidget):
         self.board_size = board_size
         self.step = min(self.height(), self.width()) / (self.board_size+1)
         self.game = GoBangLogic(width=board_size, height=board_size)
+        self.player_score = None
+        self.ai_score = None
+        self.last_u = None
+        self.last_v = None
         self.ai = GobangAi()
         self.game.start_a_game()
-        u, v, ai_score, player_score = self.ai.ai_move(self.game.board, -1, -1)
-        self.game.move_a_piece(u, v)
+        self.score_display_toggle = 0
+        # u, v, self.ai_score, self.player_score = self.ai.ai_move(self.game.board, -1, -1)
+        # self.game.move_a_piece(u, v)
 
     def paintEvent(self, QPaintEvent):
         painter = QtGui.QPainter(self)
@@ -26,11 +31,19 @@ class GameBoard(QWidget):
 
     def keyPressEvent(self, keyPressEvent):
         key = chr(keyPressEvent.key())
-        if key=='r' or key=='R':
+        if key == 'r' or key == 'R':
             self.game = GoBangLogic()
             self.ai = GobangAi()
+            self.ai_score = None
+            self.player_score = None
+            self.last_u = None
+            self.last_v = None
             self.game.start_a_game()
-            self.update()
+        if key == 'd' or key == 'D':
+            self.score_display_toggle += 1
+            self.score_display_toggle = self.score_display_toggle % 3
+        self.update()
+
 
     def drawBackground(self, painter):
         painter.setBrush(QtGui.QBrush(QtGui.QColor(128, 128, 128)))
@@ -54,7 +67,31 @@ class GameBoard(QWidget):
                     painter.setBrush(QtGui.QBrush(QtGui.QColor(255, 255, 255)))
                 painter.setPen(QtGui.QPen(QtGui.QColor(0, 0, 0, 0)))
                 painter.drawEllipse((col+0.5) * self.step, (row+0.5) * self.step, self.step, self.step)
-        # TODO Draw score on Ui
+                if self.last_u == row and self.last_v == col:
+                    painter.setBrush(QtGui.QBrush(QtGui.QColor(255, 0, 0)))
+                    painter.setPen(QtGui.QPen(QtGui.QColor(0, 0, 0, 0)))
+                    painter.drawEllipse((col + 0.75) * self.step, (row + 0.75) * self.step, self.step / 2, self.step / 2)
+
+        if self.score_display_toggle == 1:
+            score_board = self.player_score
+            color = (255, 0, 0)
+        elif self.score_display_toggle == 2:
+            score_board = self.ai_score
+            color = (0, 255, 0)
+        else:
+            score_board = None
+        if not score_board:
+            return
+        for row, line in enumerate(score_board):
+            for col, score in enumerate(line):
+                if self.game.board[row][col] != Piece_Empty:
+                    continue
+                alpha = min(255, score / 5000 * 255)
+                painter.setBrush(QtGui.QBrush(QtGui.QColor(*color, alpha)))
+                painter.setPen(QtGui.QPen(QtGui.QColor(0, 0, 0, 0)))
+                painter.drawEllipse((col + 0.5) * self.step, (row + 0.5) * self.step, self.step, self.step)
+                painter.setPen(QtGui.QPen(QtGui.QColor(255, 255, 255)))
+                painter.drawText((col + 0.8) * self.step, (row + 1) * self.step, '{}'.format(score))
 
     def mousePressEvent(self, QMouseEvent):
         x = QMouseEvent.x()
@@ -63,8 +100,8 @@ class GameBoard(QWidget):
         row = int(round(y / self.step)) - 1
         col = int(round(x / self.step)) - 1
         if self.game.move_a_piece(row, col):
-            u, v, ai_score, player_score = self.ai.ai_move(self.game.board, row, col)
-            self.game.move_a_piece(u, v)
+            self.last_u, self.last_v, self.ai_score, self.player_score = self.ai.ai_move(self.game.board, row, col)
+            self.game.move_a_piece(self.last_u, self.last_v)
             self.update()
 
 
